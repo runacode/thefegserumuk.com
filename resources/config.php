7a6e8e3e928026f1af22af93d39687bd3354ccab
runacode <59107019+runacode@users.noreply.github.com>
2020-01-11 14:09:51 -0600

    Auto move

--- resources/config.php
+++ resources/config.php
@@ -3,19 +3,20 @@
 KformConfig::setConfig(array(
     "isWordpress" => false,
     "apiLoginId" => "os_api",
-    "apiPassword" => 'p@$$w0rd123123',
-    "authString" => "39871422501d77894e0069d6646e810f",
-    "autoUpdate_allowedIps" => array("80.248.30.132"),
-    "campaignId" => 4,
-    "resourceDir" => "resources/"));
+   "authString"=>"c2f9d089f0e7ea7dde2402ffb3efebe2",
+	"autoUpdate_allowedIps"=>array("80.248.30.132"),
+	"campaignId"=>15,
+	"resourceDir"=>"resources/"));
 
 
-/*
+
+
+/* 
 !---------------------------------IMPORTANT-----------------------------------!
 
 Documentation:
-
-	-Full documentation on landing pages can be found at
+	
+	-Full documentation on landing pages can be found at 
 
 Auto-Update Feature:
 
@@ -24,28 +25,29 @@ Auto-Update Feature:
 	to keep your landing page up-to-date concerning new coupons / shipping options
 	and product changes.
 
-	-To use the campaign auto-update feature, the apache or ngix user
+	-To use the campaign auto-update feature, the apache or ngix user 
 	(depending on your httpd software) must have write access to this file
-
-	-If you are not using the auto-update feature, you will need to manually
-	replace this file after making changes to the campaign
-
+	
+	-If you are not using the auto-update feature, you will need to manually 
+	replace this file after making changes to the campaign	
+	
 !---------------------------------IMPORTANT-----------------------------------!
 */
+
 class KFormConfig
 {
-
+	
 	public $isWordpress = false;
 	public $apiLoginId = '';
 	public $apiPassword = '';
 	public $resourceDir;
 	public $baseDir;
-
-
+	
+	
 	public $mobileRedirectUrl;
 	public $desktopRedirectUrl;
-
-
+	
+	
 	public $continents;
 	public $countries;
 	public $coupons;
@@ -60,31 +62,31 @@ class KFormConfig
 	public $taxes;
 	public $termsOfService;
 	public $webPages;
-
+	
 	static $instance = NULL;
 	static $options;
 	static $campaignData;
-	// class constructor to set the variable values
-
+	// class constructor to set the variable values	
+	
 	static function setConfig($options)
 	{
-		self::$options = $options;
+		self::$options = $options;	
 	}
-
+	
 	public function __construct()
 	{
 		if(!empty(self::$instance))
 			throw new Exception("cannot recreated KFormConfig");
-
+		
 		foreach((array) self::$options as $k=>$v)
 			$this->$k = $v;
-
+			
 		if($this->isWordpress)
 		{
 			$options = get_option('konnek_options');
 			foreach((array)$options as $k=>$v)
 				$this->$k = $v;
-
+		
 			$data = json_decode(get_option('konnek_campaign_data'));
 			foreach($data as $k=>$v)
 				$this->$k = $v;
@@ -105,16 +107,16 @@ class KFormConfig
 		}
 
 		self::$instance = $this;
-
-
+		
+	
 	}
 }
 
-/*
+/* 
 !---------------------------------IMPORTANT-----------------------------------!
 
 	ABSOLUTELY DO NOT EDIT BELOW THIS LINE
-
+	
 !---------------------------------IMPORTANT-----------------------------------!
 */
 $requestUri = $_SERVER['REQUEST_URI'];
@@ -122,16 +124,16 @@ $baseFile = basename(__FILE__);
 
 if($_SERVER['REQUEST_METHOD']=='POST' && strstr($requestUri,$baseFile))
 {
-
+	
 	$authString = filter_input(INPUT_POST,'authString',FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH);
 	if(empty($authString))
 		die(); //exit silently, don't want people to know that this file processes api requests if they are just sending random posts at it
-
-
+	
+	
 	$remoteIp = $_SERVER['REMOTE_ADDR'];
 	if (isset($_SERVER["HTTP_CF_CONNECTING_IP"]))
 		  $remoteIp =  $_SERVER["HTTP_CF_CONNECTING_IP"];
-
+	
 	$allowedIps = KFormConfig::$options['autoUpdate_allowedIps'];
 	if(!in_array($remoteIp,$allowedIps))
 		die("ERROR: Invalid IP Address. Please confirm that the Konnektive IP Address is in the allowedIps array.");
@@ -175,6 +177,7 @@ if($_SERVER['REQUEST_METHOD']=='POST' && strstr($requestUri,$baseFile))
 }
 
 /*[DYNAMIC-DATA-TOKEN] do not remove */
+
 KFormConfig::$campaignData = '{
     "countries": {
         "GB": "United Kingdom"
@@ -433,11 +436,11 @@ KFormConfig::$campaignData = '{
     "webPages": {
         "catalogPage": {
             "disableBack": 0,
-            "url": "https:\/\/feg-uk.com\/"
+            "url": "https:\/\/thefegserumuk.com\/"
         },
         "checkoutPage": {
             "disableBack": 0,
-            "url": "https:\/\/feg-uk.com\/checkout.php",
+            "url": "https:\/\/thefegserumuk.com\/checkout.php",
             "autoImportLead": 1,
             "productId": null,
             "requireSig": 0,
@@ -447,33 +450,33 @@ KFormConfig::$campaignData = '{
         },
         "thankyouPage": {
             "disableBack": 0,
-            "url": "https:\/\/feg-uk.com\/thankyou.php",
+            "url": "https:\/\/thefegserumuk.com\/thankyou.php",
             "createAccountDialog": 0,
             "reorderUrl": null,
             "allowReorder": 0
         },
         "upsellPage1": {
             "disableBack": 1,
-            "url": "https:\/\/feg-uk.com\/upsell1.php",
+            "url": "https:\/\/thefegserumuk.com\/upsell1.php",
             "createAccountDialog": 0,
             "requirePayInfo": 0,
-            "productId": 52,
+            "productId": 62,
             "replaceProductId": null
         },
         "upsellPage2": {
             "disableBack": 1,
-            "url": "https:\/\/feg-uk.com\/upsell2.php",
+            "url": "https:\/\/thefegserumuk.com\/upsell2.php",
             "createAccountDialog": 0,
             "requirePayInfo": 0,
-            "productId": 51,
+            "productId": 61,
             "replaceProductId": null
         },
         "upsellPage3": {
             "disableBack": 1,
-            "url": "https:\/\/feg-uk.com\/upsell3.php",
+            "url": "https:\/\/thefegserumuk.com\/upsell3.php",
             "createAccountDialog": 0,
             "requirePayInfo": 0,
-            "productId": 53,
+            "productId": 63,
             "replaceProductId": null
         },
         "productDetails": {
@@ -481,27 +484,27 @@ KFormConfig::$campaignData = '{
         }
     },
     "landerType": "CART",
-    "googleTrackingId": "UA-155623562-6",
+    "googleTrackingId": "UA-155623562-10",
     "enableFraudPlugin": 0,
     "autoTax": 0,
     "taxServiceId": null,
     "companyName": "optin_solutions_llc",
     "offers": {
-        "49": {
-            "productId": 49,
+        "59": {
+            "productId": 59,
             "name": "Feg Serum - Eyelash Enhancer",
             "description": "*No description available",
-            "imagePath": "https:\/\/feg-uk.com\/resources\/images\/smain-small.jpg",
+            "imagePath": "https:\/\/thefegserumuk.com\/resources\/images\/smain-small.jpeg",
             "imageId": 1,
             "price": "11.97",
             "shipPrice": "0.00",
             "category": "FEG"
         },
-        "50": {
-            "productId": 50,
+        "60": {
+            "productId": 60,
             "name": "Feg Serum - Eyelash Enhancer - Free",
             "description": "*No description available",
-            "imagePath": "https:\/\/feg-uk.com\/resources\/images\/smain-small.jpg",
+            "imagePath": "https:\/\/thefegserumuk.com\/resources\/images\/smain-small.jpeg",
             "imageId": 1,
             "price": "0.00",
             "shipPrice": "0.00",
@@ -509,31 +512,31 @@ KFormConfig::$campaignData = '{
         }
     },
     "upsells": {
-        "51": {
-            "productId": 51,
+        "61": {
+            "productId": 61,
             "name": "Feg Serum - Eyelash Enhancer - Free Gift",
             "description": "*No description available",
-            "imagePath": "https:\/\/feg-uk.com\/resources\/images\/upsell1.jpg",
+            "imagePath": "https:\/\/thefegserumuk.com\/resources\/images\/upsell1.jpg",
             "imageId": 1,
             "price": "4.95",
             "shipPrice": "0.00",
             "category": "FEG"
         },
-        "52": {
-            "productId": 52,
+        "62": {
+            "productId": 62,
             "name": "FEG - EyeBrown (2pcs - 2 months of treatment)",
             "description": "*No description available",
-            "imagePath": "https:\/\/feg-uk.com\/resources\/images\/upsell2.jpg",
+            "imagePath": "https:\/\/thefegserumuk.com\/resources\/images\/upsell2.jpg",
             "imageId": 2,
             "price": "9.95",
             "shipPrice": "0.00",
             "category": "FEG"
         },
-        "53": {
-            "productId": 53,
+        "63": {
+            "productId": 63,
             "name": "Silicone Make-Up Sponge",
             "description": "*No description available",
-            "imagePath": "https:\/\/feg-uk.com\/resources\/images\/upsell3.jpg",
+            "imagePath": "https:\/\/thefegserumuk.com\/resources\/images\/upsell3.jpg",
             "imageId": 3,
             "price": "4.95",
             "shipPrice": "0.00",
